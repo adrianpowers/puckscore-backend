6755dbf8e425dcab97e653db04a875f1248b5116
Adrian Powers <adrianpowers530@gmail.com>
2024-03-30 01:39:51 -0400

    Minor updates to formatting and schema

--- models/match.js
+++ models/match.js
@@ -4,6 +4,7 @@ const matchSchema = new mongoose.Schema({
   players: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Player' }],
   date: { type: Date, default: Date.now },
   outcome: String,
+  inProgress: true,
   sets: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Set' }]
 });
 
--- routes/playerRoutes.js
+++ routes/playerRoutes.js
@@ -1,15 +1,15 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const playerController = require('../controllers/playerController');
+const playerController = require("../controllers/playerController");
 
-router.get('/', playerController.getAllPlayers);
-router.get('/:name', playerController.getPlayerByName);
-router.get('/:id', playerController.getPlayerById);
-router.post('/', playerController.createPlayer);
+router.get("/", playerController.getAllPlayers);
+router.get("/:name", playerController.getPlayerByName);
+router.get("/:id", playerController.getPlayerById);
+router.post("/", playerController.createPlayer);
 router.put("/:id/name", playerController.updatePlayerName);
 router.put("/:id/callsign", playerController.updatePlayerCallsign);
 router.put("/:id/state-rank", playerController.updatePlayerStateRank);
 router.put("/:id/world-rank", playerController.updatePlayerWorldRank);
-router.delete('/:id', playerController.deletePlayer);
+router.delete("/:id", playerController.deletePlayer);
 
 module.exports = router;
